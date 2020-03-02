43d2c98c5300cf4a6d6e42e3ea9055520a056965
Ishan Bhatt <ishanbhatt@halwa>
2020-03-02 13:30:45 +0530

    Added codes for get set in convention, IMP note about rangeTo function it clears doubts about type T

--- src/Chapter7/OverLoadEx.kt
+++ src/Chapter7/OverLoadEx.kt
@@ -1,11 +1,11 @@
 package Chapter7
 
 
-import Chapter4.Person
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-data class Point(val x: Int, val y: Int) {
+// x,y must be val but I changed it to var so that I can support set method
+data class Point(var x: Int, var y: Int) {
     // COuld have done this.x but that is optional
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
     operator fun minus(other: Point) = sqrt((x - other.x).toDouble().pow(2) + (y - other.y).toDouble().pow(2))
@@ -26,13 +26,41 @@ operator fun Point.times(scale: Int) = Point(x * scale, y * scale)
 // Unary minus operator
 operator fun Point.unaryMinus() = Point(-x, -y)
 
+// Accessing elements by index get and set
+// CHECK THE POSSIBILITY OF USING INHERITANCE FOR 3RD DIMENSION
+// point[index] will get translated into get method
+operator fun Point.get(index: Int) = when(index) {
+    0 -> x
+    1 -> y
+    else -> throw IndexOutOfBoundsException("Only 2 dimension points are supported")
+}
+
+// Here's a cool trick, using when to assign something / change something,
+// Previously we were doing just return expression and other things
+// Also, if you needed something like x[a,b] = c -> x.set(a,b,c)
+operator fun Point.set(index: Int, value: Int) {
+    when(index) {
+        0 -> x = value
+        1 -> y = value
+        else -> throw IndexOutOfBoundsException("Only 2 dimension points are supported")
+    }
+}
+
 class ComparablePerson(private val firstName: String, private val lastName: String) : Comparable<ComparablePerson> {
     override fun compareTo(other: ComparablePerson): Int {
         return compareValuesBy(this, other, ComparablePerson::firstName, ComparablePerson::lastName)
-        // This function first compare it with firstName then with the lastName
+        // This function first compare it with firstName then with the lastName and so on
     }
 
 }
+
+// The in convention, Say Point(1,2) in SOME RECTANGLE, It is similar to checking if element is in list or map
+data class Rectangle(val upperLeft: Point, val lowerRight: Point)
+
+operator fun Rectangle.contains(point: Point): Boolean {
+    return point.x in upperLeft.x until lowerRight.x && point.y in upperLeft.y until lowerRight.y
+}
+
 
 fun main() {
     val p1 = Point(3,4)
@@ -57,4 +85,13 @@ fun main() {
     val per1 = ComparablePerson("Alan", "Smith")
     val per2 = ComparablePerson("Bruce", "Wayne")
     println(per1 < per2)
+
+    println("X co-ordinate of p1 is ${p1[0]}")
+    p1[0] = 25
+    println("CHANGED X co-ordinate of p1 is ${p1[0]}")
+
+    println("CHECKING THE IN")
+    val rect = Rectangle(Point(10, 20), Point(50, 50))
+    println(Point(20,30) in rect)  // Here, Point(20,30) is passed as an argument to function contains
+    println(Point(5,5) in rect)
 }
