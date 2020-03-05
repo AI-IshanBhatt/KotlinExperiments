6d2d72d2c0fd43c99b7da35ec4e709daa3c7e515
Ishan Bhatt <ishanbhatt@halwa>
2020-03-05 17:56:08 +0530

    SiteVisit thing done

--- src/Chapter8/HigherOrderEx.kt
+++ src/Chapter8/HigherOrderEx.kt
@@ -39,6 +39,38 @@ fun String.filter(predicate: (Char) -> Boolean): String {
     return sb.toString()
 }
 
+// RETURNING A FUNCTION FROM FUNCTION
+enum class Delivery { STANDARD, EXPEDITED }
+
+class Order(val itemCount: Int)
+
+// It will return a function that takes Order and returns it's shippingCost in double
+fun getShippingCostCalculator(delivery: Delivery) : (Order) -> Double {
+    if (delivery == Delivery.EXPEDITED) return { order ->  6 + 4.0*order.itemCount}
+    else return { order -> 2.0*order.itemCount }
+}
+
+// Suppose for a contact list, you want to check if the person name starts with a prefix and/or he has number or not
+data class UIPerson(val firstName: String, val lastName: String, val phoneNumber: String?)
+
+// When a user types D to see the contacts whose first or last name starts with D, the prefix value is updated.
+class ContactListFilters {
+    var prefix: String = ""
+    var onlyWithNumber: Boolean = false
+
+    fun getPredicate(): (UIPerson) -> Boolean {
+        val startsWithPrefix = { p: UIPerson -> p.firstName.startsWith(prefix) || p.lastName.startsWith(prefix) }
+
+        if (!onlyWithNumber) return startsWithPrefix  // Returns a variable of a function type
+        return {
+            startsWithPrefix(it) && it.phoneNumber!=null  // Returns a Lambda, but it is smart enough to figure out that it is a UIPerson
+                                                          // As we had specified the return type as (UIPerson) -> Boolean, it infers that the lambda would be of same signature
+                                                          // So, it has to be UIPerson (captured variable)
+        }
+    }
+}
+
+
 fun main() {
     twoAndThree {a, b ->  a+b}
     twoAndThree {a, b -> a-b}
@@ -52,5 +84,19 @@ fun main() {
     val numbers = listOf(1,2,3,4,5)
     println("Squares ${numbers.joiningString(seperator = "-"){ i ->  (i*i).toString()}}")
 
+    val calculator = getShippingCostCalculator(Delivery.EXPEDITED)
+    val shippingCost = calculator(Order(10))
 
+    println("Shipping cost of Expedited Delivery is $shippingCost")
+
+    val contacts = listOf(UIPerson("Dmitry", "Jemerov", "123-4567"), UIPerson("Svetlana", "Isakova", null))
+
+    val contactListFilters = ContactListFilters()
+    with(contactListFilters) {
+        prefix = "D"
+        onlyWithNumber = true
+    }
+
+//    println(contacts.filter(contactListFilters.getPredicate()))  // Passing a function returned by getPredicate as an argument to filter
+    println(contacts.filter { contactListFilters.getPredicate()(it) })  // Accept that above line looks better
 }
