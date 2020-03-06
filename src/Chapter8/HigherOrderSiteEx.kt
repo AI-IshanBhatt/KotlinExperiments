b7426c49cac40af211537dbd10d4311bdbbd1434
AI-Ishanbhatt <ishan_bhatt@hotmail.com>
2020-03-06 06:47:43 +0530

    Finished higher order function, added a note about inlining CODE and other explanation is coming up

--- src/Chapter8/HigherOrderSiteEx.kt
+++ src/Chapter8/HigherOrderSiteEx.kt
@@ -14,4 +14,20 @@ fun main() {
         SiteVisit("/", 16.3, OS.ANDROID)
     )
 
+    val naiveWindowsDuration = log.filter { it.os == OS.WINDOWS }.map { it.duration }.average()
+    println("AVERAGE DURATION ON WINDOWS MACHINE $naiveWindowsDuration")
+
+    // Ypu may want similar stats for other as well, I am sure you will not repeat it
+    // If you had used map() -> Use this :: style, for map {} , use that it. style
+    fun List<SiteVisit>.averageDurationFor(os: OS) = filter { it.os == os }.map(SiteVisit::duration).average()
+
+    println("MAC AVG ${log.averageDurationFor(OS.MAC)}")
+
+    // Let's assume you want average duration for signup page OR mobile devices etc
+    fun List<SiteVisit>.genericAverage(predicate: (SiteVisit) -> Boolean) =
+        filter(predicate).map(SiteVisit::duration).average()
+
+    println("AVERAGE DURATION FOR MOBILE ${log.genericAverage { it.os in setOf(OS.ANDROID, OS.IOS) }}")
+    println("AVERAGE DURATION FOR ROOT PAGE ${log.genericAverage { it.path == "/" }}")
+
 }
